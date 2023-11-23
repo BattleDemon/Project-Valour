5a77442cca0f240711809c0cf6659f13474938fc
BattleDemon <143239271+BattleDemon@users.noreply.github.com>
2023-11-24 00:42:48 +1100

    added camera movement

--- UI/Camera2D.gd
+++ UI/Camera2D.gd
@@ -10,11 +10,16 @@ var endV = Vector2()
 var isDragging = false
 signal area_selected
 signal start_move_selection
+var is_moving_up = false
+var is_moving_down = false
+var is_moving_left = false
+var is_moving_right = false
 
 @onready var box = get_node("../UI/Panel")
 
 func _ready():
 	connect("area_selected", Callable(get_parent(), "_on_area_selected"))
+
 
 func _process(delta):
 	if Input.is_action_just_pressed("LeftClick"):
@@ -38,6 +43,30 @@ func _process(delta):
 			end = start
 			isDragging = false
 			draw_area(false)
+	if is_moving_down == true:
+		if self.zoom > Vector2(1.25,1.25):
+			self.position = self.position + Vector2(0,10) 
+		else:
+			self.position = self.position + Vector2(0,10) * (Vector2(.5,.5) + self.zoom)
+		
+	if is_moving_up == true:
+		if self.zoom > Vector2(1.25,1.25):
+			self.position = self.position - Vector2(0,10)
+		else:
+			self.position = self.position - Vector2(0,10) * (Vector2(.5,.5) + self.zoom)
+		
+	if is_moving_left == true:
+		if self.zoom > Vector2(1.25,1.25):
+			self.position = self.position - Vector2(10,0)
+		else:
+			self.position = self.position - Vector2(10,0) * (Vector2(.5,.5) + self.zoom)
+		
+	if is_moving_right == true:
+		if self.zoom > Vector2(1.25,1.25):
+			self.position = self.position + Vector2(10,0)
+		else:
+			self.position = self.position + Vector2(10,0) * (Vector2(.5,.5) + self.zoom)
+
 
 func _input(event):
 	if event is InputEventMouse:
@@ -50,7 +79,31 @@ func _input(event):
 			elif event.is_action("Scroll down"):
 				if self.zoom > Vector2(.5, .5):
 					self.zoom = self.zoom - Vector2(.1, .1)
-				
+					
+	if event.is_action_pressed("W"):
+		is_moving_up = true
+		
+	elif event.is_action_pressed("A"):
+		is_moving_left = true
+		
+	elif event.is_action_pressed("D"):
+		is_moving_right = true
+		
+	elif event.is_action_pressed("S"):
+		is_moving_down = true
+		
+	if event.is_action_released("W"):
+		is_moving_up = false
+		
+	elif event.is_action_released("A"):
+		is_moving_left = false
+		
+	elif event.is_action_released("D"):
+		is_moving_right = false
+		
+	elif event.is_action_released("S"):
+		is_moving_down = false
+		
 
 func draw_area(s=true):
 	box.size = Vector2(abs(startV.x - endV.x), abs(startV.y - endV.y))
